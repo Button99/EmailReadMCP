d78edc3d9b268e54de117a908b7dabcc5b91664e
Button99 <christoskoubis@yahoo.com>
2025-11-09 13:34:10 +0200

    Update Template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
     </div>
 
     <div class="footer">
-        <p>&copy; {{ date('Y') }} Your Company. All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} This was an AI generated email.</p>
     </div>
 </div>
 </body>
